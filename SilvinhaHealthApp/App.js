b92f29a061ca8610885ec06a400965bf7922c452
matheusf21 <matheusferreiradelima21@gmail.com>
2025-02-19 13:59:48 -0300

    Criado projeto

---
+++ SilvinhaHealthApp/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Silvinha Health App</Text>
+      </View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
